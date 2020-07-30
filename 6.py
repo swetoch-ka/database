08a2aaf4122580f3ccb5691efdf70660c735fdec
Svetlanka <swet.lan.ka.r@gmail.com>
2020-07-31 02:35:16 +0300

    Pyhton_Lesson_3

---
+++ 6.py
@@ -0,0 +1,26 @@
+# 6. Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же, но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
+# Продолжить работу над заданием. В программу должна попадать строка из слов, разделенных пробелом.
+# Каждое слово состоит из латинских букв в нижнем регистре. Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
+# Необходимо использовать написанную ранее функцию int_func().
+
+# Вариант 1
+def int_func_1(var_word):
+    return var_word.title()
+
+
+print(int_func_1("word"))
+
+
+# Вариант 2
+def int_func_2(var_word):
+    separate_word = var_word.split(' ')
+    total = []
+    for i in separate_word:
+        string_element = str(i)
+        first_letter = string_element[:1].upper()
+        word = first_letter + string_element[1:]
+        total.append(word)
+    return ' '.join(total)
+
+
+print(int_func_2("few words"))
